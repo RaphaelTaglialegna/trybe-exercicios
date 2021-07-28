0ce5e3876da3a19d3da30ce92ee9707fb00a0361
Raphael Taglialegna <raphael.trcosta@gmail.com>
2021-07-28 19:25:49 -0300

    Exercícios de Prática

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/Pratica 1 /partica1.js
@@ -0,0 +1,10 @@
+const myName = "Raphael";
+const birthCity = "Campinas";
+let bithYear = 1987;
+bithYear = 2030;
+birthCity = "Jaú";
+
+console.log(myName);
+console.log(birthCity);
+console.log(bithYear);
+
