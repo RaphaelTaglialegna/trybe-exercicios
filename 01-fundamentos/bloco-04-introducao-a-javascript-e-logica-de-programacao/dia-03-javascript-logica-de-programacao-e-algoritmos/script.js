b138b0acce0915699d362d7c5ba29bbb57e4a0d9
Raphael Taglialegna <raphael.trcosta@gmail.com>
2021-08-03 10:52:06 -0300

    Exercícios Logica de Programação

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/script.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -36,3 +36,87 @@ for(let i2 = 1; i2 <= n; i2 += 1){
   }  
   console.log(space + print2);
 }
+
+// //4- Depois, faça uma pirâmide com n asteriscos de base.
+// console.log(); 
+
+// let n1 = 5;
+// let symbol ='*';
+// let printLine ='';
+
+// //The first '*' is in the center of the tryangle. 
+// let midle = (n1 + 1) / 2;
+
+// //We have to use a ' ' (empty space) in bouth sides to control, which is using the midle position to control += and -=;
+// let empleft = midle;
+// let empright = midle;
+
+//  for(let i3 = 0; i3 < midle; i3 += 1){
+//   for(let c3 = 0; c3 <= n1; c3 += 1){
+//     if(c3 >= empleft && c3 <= empright){
+//       printLine = printLine + symbol;
+//     }else{
+//     printLine = printLine + ' ';
+//      }
+  
+//   }
+//   console.log(printLine);
+//   printLine = '';
+//   empleft -= 1;
+//   empright += 1; 
+// }
+
+//5- Faça uma pirâmide com n asteriscos de base que seja vazia no meio. Assuma que o valor de n será sempre ímpar:
+console.log(); 
+
+let n1 = 7;
+let symbol ='*';
+let printLine ='';
+
+//The first '*' is in the center of the tryangle. 
+let midle = (n1 + 1) / 2;
+
+//We have to use a ' ' (empty space) in bouth sides to control, which is using the midle position to control += and -=;
+let empleft = midle;
+let empright = midle;
+
+ for(let i3 = 1; i3 <= midle; i3 += 1){
+  for(let c3 = 1; c3 <= n1; c3 += 1){
+    if(c3 == empleft || c3 == empright || i3 == midle){ // compare one case to print the symbol. 
+      printLine +=symbol;
+    }else{
+    printLine += ' ';
+     }
+  
+  }
+  console.log(printLine);
+  printLine = '';
+  empleft -= 1;
+  empright += 1; 
+}
+
+
+// 6- Faça um programa que diz se um número definido numa variável é primo ou não.
+//Um número primo é um número que só é divisível por 1 e por ele mesmo, ou seja, a divisão dele com quaisquer outros números dá resto diferente de zero.
+//Dica: você vai precisar de fazer um loop que vá de 0 ao número definido; Além disso, vai precisar de fazer uma checagem a cada iteração e armazenar os resultados em algum lugar.
+
+let numberP = 7;
+let countP =[];
+let primo = 0
+for( let index = 2; index < numberP; index += 1){
+  let difference = numberP % index; 
+  countP.push(difference);
+}
+
+for (let check = 0; check < countP.length; check += 1 ){
+  if (countP[check] === 0){
+    primo += 1; 
+  } 
+}  
+
+if( primo === 0){
+  console.log('O numero '+ numberP + ' é primo');
+}else {
+  console.log('O numero '+ numberP +' não é primo');
+}
+  
