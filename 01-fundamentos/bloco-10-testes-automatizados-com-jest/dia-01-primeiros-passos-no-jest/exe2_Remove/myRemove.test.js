5a0883eac99bcb64b0d788b2d4af3ece30401667
Raphael Taglialegna <raphael.trcosta@gmail.com>
2021-09-22 15:50:17 -0300

    Exercicios Part 1 - Bloco 10.1

---
+++ 01-fundamentos/bloco-10-testes-automatizados-com-jest/dia-01-primeiros-passos-no-jest/exe2_Remove/myRemove.test.js
@@ -0,0 +1,25 @@
+// Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado
+// Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
+// Verifique se o array passado por parâmetro não sofreu alterações
+// Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado
+const myRemove = require('./myRemove');
+
+describe('Ex-2 Verifica os retornor de MyRemove', () => {
+  it('Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+  });
+  it('Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+  });
+
+  const myList = [5, 6, 7, 8];
+  myRemove(myList, 5);
+
+  it('Verifique se o array passado por parâmetro não sofreu alterações', () => {
+    expect((myList)).toEqual([5, 6, 7, 8]);
+  });
+  it('Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado', () => {
+    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+  });
+
+});
