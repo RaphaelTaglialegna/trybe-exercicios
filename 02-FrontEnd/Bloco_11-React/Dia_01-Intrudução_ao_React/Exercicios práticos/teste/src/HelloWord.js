9b960b9cb9a47f88349c3dfa784381c60dd8a845
Raphael Taglialegna <raphael.trcosta@gmail.com>
2021-10-04 16:18:49 -0300

    Exercicios Praticos React

---
+++ 02-FrontEnd/Bloco_11-React/Dia_01-Intrudução_ao_React/Exercicios práticos/teste/src/HelloWord.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+
+class HelloWord extends Component {
+  render() {
+    return <div>
+      <h1>Raphael Taglialegna</h1>
+      <p>Somos Todos Trybers!</p>
+    </div> 
+  }
+}
+
+export default HelloWord;
