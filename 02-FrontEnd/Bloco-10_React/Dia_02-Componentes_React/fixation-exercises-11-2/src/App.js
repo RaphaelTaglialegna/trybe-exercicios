11c2e607159a3b65d770705a20c144cfecc65d39
Raphael Taglialegna <raphael.trcosta@gmail.com>
2021-10-05 21:22:26 -0300

    Exercicios componentes React

--- 02-FrontEnd/Bloco-10_React/Dia_02-Componentes_React/fixation-exercises-11-2/src/App.js
+++ 02-FrontEnd/Bloco-10_React/Dia_02-Componentes_React/fixation-exercises-11-2/src/App.js
@@ -1,12 +1,30 @@
-import Image from './component/image';
+import React from 'react';
 import './App.css';
+import UserProfile from './component/UserProfile';
 
-const linkImage = 'https://images.pexels.com/photos/756790/pexels-photo-756790.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+class App extends React.Component {
+  render() {
+    const joao = {
+      id: 102,
+      name: "João",
+      email: "joao@gmail.com",
+      avatar: "https:\/\/cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_640.png"
+    };
 
-function App() {
-  return (
-    <Image source={linkImage} alternativeText='Uma bela foto do Canadá' />
-  );
+    const amelia = {
+      id: 77,
+      name: "Amélia",
+      email: "amelia@gmail.com",
+      avatar: "https:\/\/cdn.pixabay.com/photo/2017/01/31/13/05/cameo-2023867_640.png"
+    };
+
+    return (
+      <div className="App">
+        <UserProfile user={joao} />
+        <UserProfile user={amelia} />
+      </div>
+    );
+  }
 }
 
 export default App;
