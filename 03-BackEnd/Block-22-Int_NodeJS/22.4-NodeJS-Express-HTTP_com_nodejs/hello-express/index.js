eef5446a72ee7a971c675060f51c519f0b16bde4
Raphael Taglialegna <raphae.trcosta@gmail.com>
2022-02-10 09:41:32 -0300

    Express First Hello World

---
+++ 03-BackEnd/Block-22-Int_NodeJS/22.4-NodeJS-Express-HTTP_com_nodejs/hello-express/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express(); 
+
+app.get('/hello', hendleHelloWorldRquest); 
+
+app.listen(3001, () => { 
+  console.log('Aplicação ouvindo na porta 3031');
+});
+
+function hendleHelloWorldRquest(req, res) { 
+  res.status(200).send('Hello World !!!');
+
+};
+
+
