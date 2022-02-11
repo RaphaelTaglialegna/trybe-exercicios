afd3ec01fa7cd5b68740a440d7debbe463a62ec3
RaphaelTaglialegna <raphael.trcosta@gmail.com>
2022-02-11 11:34:50 -0300

    Exercicio receita

--- 03-BackEnd/Block-22-Int_NodeJS/22.4-NodeJS-Express-HTTP_com_nodejs/recipes/BACKEND/index.js
+++ 03-BackEnd/Block-22-Int_NodeJS/22.4-NodeJS-Express-HTTP_com_nodejs/recipes/BACKEND/index.js
@@ -68,10 +68,36 @@ app.get('/recipes/search', function (req, res) {
   const { name, maxPrice } = req.query;
   const filteredRecipes = recipes.filter((r) => r.name.includes(name) && r.price < parseInt(maxPrice));
   res.status(200).json(filteredRecipes);
-})
+});
 
+app.put('/recipes/:id', function (req, res) {
+  const { id } = req.params;
+  const { name, price } = req.body;
+  const recipeIndex = recipes.findIndex((r) => r.id === parseInt(id));
+
+  if (recipeIndex === -1) return res.status(404).json({ message: 'Recipe not found!' });
+
+  recipes[recipeIndex] = { ...recipes[recipeIndex], name, price };
+
+  res.status(204).end();
+});
+
+app.delete('/recipes/:id', function (req, res) {
+  const { id } = req.params;
+  const recipeIndex = recipes.findIndex((r) => r.id === parseInt(id));
+
+  if (recipeIndex === -1) return res.status(404).json({ message: 'Recipe not found!' });
+
+  recipes.splice(recipeIndex, 1);
+
+  res.status(204).end();
+});
+
+app.all('*', function (req, res) {
+  return res.status(404).json({ message: `Rota '${req.path}' não existe!`});
+});
 
 
 app.listen(3001, () => {
-  console.log('Aplicação ouvindo na porta 3001');
+  console.log('Aplicação ouvindo na porta 3001')
 });
