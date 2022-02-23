11046dce5578ae395bde4be43e6b6a17f6ba288b
RaphaelTaglialegna <raphael.trcosta@gmail.com>
2022-02-23 13:08:50 -0300

    Exercicios de Test index.js 1.0

--- 03-BackEnd/Block-23-Node.js:-Camada_de_Serviço_e_Arquitetura_Rest_e_Restful/23.4-Arquitetura_de_Software-Testando_Camadasl/movies-test/index.js
+++ 03-BackEnd/Block-23-Node.js:-Camada_de_Serviço_e_Arquitetura_Rest_e_Restful/23.4-Arquitetura_de_Software-Testando_Camadasl/movies-test/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(bodyParser.json());
 
 app.post('/movies', MovieController.create);
-app.get('/movies/:id', MovieController.findById);
+app.get('/movies/:id', MovieController.findById );
 
 
 const PORT = process.env.PORT || 3000;
