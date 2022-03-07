21f0f681bfa4c98a7e5b184014b14034c1196cee
RaphaelTaglialegna <raphael.trcosta@gmail.com>
2022-03-07 08:46:55 -0300

    NEW: Associations and tests

--- 03-BackEnd/Block-24-Node.js:ORM_e_Autenticacao/24.2-ORM-Associations/sequelize-test-employ/config/config.js
+++ 03-BackEnd/Block-24-Node.js:ORM_e_Autenticacao/24.2-ORM-Associations/sequelize-test-employ/config/config.js
@@ -2,17 +2,17 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: "root",
-    password: "12345678",
-    database: "associations",
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
     host: process.env.HOSTNAME,
     dialect: 'mysql',
   },
   test: {
-    username: "root",
-    password: "12345678",
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
     database: "orm_assoc_test_db",
-    host: "127.0.0.1",
+    host: process.env.HOSTNAME,
     dialect: 'mysql',
     logging: false
   },
